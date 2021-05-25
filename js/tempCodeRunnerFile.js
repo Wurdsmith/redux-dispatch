428325786448276454ec34de64af7b90cc09c21e
Wurdsmith <wurdsmithing@gmail.com>
2021-05-25 12:17:44 -0500

    commit tree 1c893304d4f1ed659f832bef8958f3eafd4b2ebd

---
+++ js/tempCodeRunnerFile.js
@@ -0,0 +1,16 @@
+function changeState(state, action){
+  switch (action.type) {
+    case 'INCREASE_COUNT':
+      return {count: state.count + 1}
+    default:
+      return state;
+  }
+}
+
+let state = {count: 0}
+let action = {type: 'INCREASE_COUNT'}
+
+changeState(state, action)
+console.log(changeState(state, action))
+console.log(changeState(state, action))
+console.log(changeState(state, action))
